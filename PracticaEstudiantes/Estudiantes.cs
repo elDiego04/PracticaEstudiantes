550a908478c1ae3ff42a58c7cb9132654560483f
Diego Salazar <diego.salazarc4@gmail.com>
2023-08-29 17:46:46 -0500

    fourth commit

--- PracticaEstudiantes/Estudiantes.cs
+++ PracticaEstudiantes/Estudiantes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 
 
@@ -25,10 +26,11 @@ class Estudiantes
         this.nota3 = 0.0;
         this.programa = "Sistemas";
         this.edad = 17;
-        Informe();
+
     }
 
-    public Estudiantes(string nombre, string apellido, double nota1, double nota2, double nota3, string programa, int edad)
+    public Estudiantes(string nombre, string apellido, double nota1, double nota2, double nota3, string programa,
+        int edad)
     {
         this.nombre = nombre;
         this.apellido = apellido;
@@ -37,7 +39,7 @@ class Estudiantes
         this.nota3 = nota3;
         this.programa = programa;
         this.edad = edad;
-        Informe();
+
     }
 
     public Estudiantes(string nombre, string apellido, double nota1, double nota2, double nota3)
@@ -49,62 +51,79 @@ class Estudiantes
         this.nota3 = nota3;
         this.programa = "Sistemas";
         this.edad = 17;
-        Informe();
+
     }
 
+    public Estudiantes(string nombre, string apellido)
+    {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        Write("\nIngrese nota 1: ");
+        this.nota1 = Convert.ToDouble(nota1);
+        Write("\nIngrese nota 2: ");
+        this.nota2 = Convert.ToDouble(nota1);
+        Write("\nIngrese nota 3: ");
+        this.nota3 = Convert.ToDouble(nota1);
+        Write("\nIngrese programa: ");
+        this.programa = ReadLine();
+        Write("\nIngrese nota 1: ");
+        this.edad = Convert.ToInt32(edad);
+
+    }
 
     public string Saludar()
     {
-        TimeSpan horaActual = DateTime.Now.TimeOfDay;
-        if (horaActual < TimeSpan.FromHours(12))
-        {
-            return $"Buenos dias {nombre} {apellido}, ¡Bienvenido al programa de {programa}!";
-        }
-        else if (horaActual < TimeSpan.FromHours(18))
-        {
-            return $"Buenos tardes {nombre} {apellido}, ¡Bienvenido al programa de {programa}!";
-        }
-        else
-        {
-            return $"Buenos noches {nombre} {apellido}, ¡Bienvenido al programa de {programa}!";
-        }
+        int hora = DateTime.Now.Hour;
+        if (this.nombre.Equals("Ninguno")) this.nombre = "";
+        if (this.apellido.Equals("Ninguno")) this.apellido = "";
+        if (hora > 0 && hora < 12)
+            return "Buenos días " + this.nombre + " " + this.apellido + " estudiante de " + this.programa;
+        else if (hora >= 12 && hora < 18)
+            return "Buenas tardes" + this.nombre + " " + this.apellido + " estudiante de " + this.programa;
+        return "Buenas noches " + this.nombre + " " + this.apellido + " estudiante de " + this.programa;
 
     }
+
     public double Definitiva()
     {
-        return (this.nota1 + this.nota2 + this.nota3) / 3;
+        return Math.Round((this.nota1 + this.nota2 + this.nota3) / 3, 2);
     }
+
     public int Clasificar()
     {
-        if (this.Definitiva() <= 2.0) { return 1; }
-        else if (this.Definitiva() <= 4.0) { return 2; }
-        else { return 3; }
-
+        double def = this.Definitiva();
+        if (def <= 2.0)
+            return 1;
+        else if (def <= 4.0)
+            return 2;
+        return 3;
     }
+
     public string Citar()
     {
-        if (this.Clasificar() == 1 && this.edad < 18 && this.programa == "Sistemas") { return "DEBE CITARSE AL ACUDIENTE "; }
-        else { return "NO ES NECESARIO CITARSE AL ACUDIENTE"; }
+        if (this.Clasificar() == 1 && this.edad < 18 && this.programa.Equals("Sistemas"))
+            return "DEBE CITARSE AL ACUDIENTE ";
+        return "NO ES NECESARIO CITARSE AL ACUDIENTE";
 
     }
 
-    public void Informe()
+    public void Informe(string mensaje)
     {
-        string mensajeSaludo = Saludar();
-        double definitiva = Math.Round(Definitiva(), 2);
-        int clasificacion = Clasificar();
-        string mensajeCita = Citar();
+        WriteLine("\n======================================================");
+        BackgroundColor = ConsoleColor.DarkBlue;
+        WriteLine($"       --> Información {mensaje}  <--             ");
+        BackgroundColor = ConsoleColor.Black;
+        WriteLine("======================================================");
+        WriteLine(this.Saludar());
+        if (!this.nombre.Equals(""))
+        {
+            WriteLine($"Edad Estudiante: {this.edad}");
+            WriteLine(
+                $"Definitiva     : {this.Definitiva():n} Nota1: {this.nota1:n1} Nota2: {this.nota2:n1} Nota3: {this.nota3:n1}");
+            WriteLine($"Clasificación  : {this.Clasificar()}");
+            WriteLine(this.Citar());
+        }
 
-        Console.WriteLine(mensajeSaludo);
-        Console.WriteLine($"Definitiva: {definitiva}");
-        Console.WriteLine($"Clasificación: {clasificacion}");
-        Console.WriteLine($"Cita: {mensajeCita}");
-        Console.WriteLine("NOTAS: ");
-        Console.WriteLine($"NOTA 1: {nota1}");
-        Console.WriteLine($"NOTA 2: {nota2}");
-        Console.WriteLine($"NOTA 3: {nota3}");
-        Console.WriteLine($"EDAD: {edad}");
     }
 }
-
 
--- PracticaEstudiantes/Program.cs
+++ PracticaEstudiantes/Program.cs
@@ -10,10 +10,17 @@ class Program
     static void Main(string[] args)
     {
         Estudiantes estudiante1 = new Estudiantes();
+        estudiante1.Informe("Estudiante #1");
+        Estudiantes estudiante2 = new Estudiantes("Jairo", "Ramirez", 5.0, 4.0, 3.0, "Electrónica", 46);
+        estudiante2.Informe("Estudiante #2");
+        Estudiantes estudiante3 = new Estudiantes("Alejandra", "García", 2.5, 2.2, 0.5);
+        estudiante3.Informe("Estudiante #3");
+        Estudiantes estudiante4 = new Estudiantes("Juan", "Pérez");
+        estudiante4.Informe("Estudiante #4");
+        Estudiantes estudiante5 = new Estudiantes("Hernan", "Marin", 1.0, 2.8, 3.0, "Sistemas", 15);
+        estudiante5.Informe("Estudiante #5");
 
-        Estudiantes estudiante2 = new Estudiantes("Diego", "Salazar", 4.5, 3.0, 5.0, "Sistemas", 19);
 
-        Estudiantes estudiante3 = new Estudiantes("Gladys", "Castrillon", 1.0, 2.0, 3.0);
     }
 }
 
